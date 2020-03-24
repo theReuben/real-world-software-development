07210b60ba0ee23689ba2e44967ec729bc3d10e6
Reuben Day <reubenday@btinternet.com>
2020-03-24 13:55:17 +0000

    Renamed files, moved {

--- my-app/src/test/java/com/thereuben/rwsd/AppTest.java
+++ my-app/src/test/java/com/thereuben/rwsd/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.thereuben.rwsd;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,8 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
